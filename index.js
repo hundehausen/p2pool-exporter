6bd6bf55c752f8be65dd34cd2080b8a7106d591f
hundehausen <papier@grischadaum.de>
2023-02-24 11:27:30 +0100

    top level await

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import * as promClient from "prom-client";
-import fetch from "node-fetch";
 import "dotenv/config";
 
 const PORT = process.env.PORT || 6543;
@@ -31,33 +30,29 @@ const unclesGauge = new Gauge({
   help: "Number of uncles",
 });
 
-const main = async () => {
-  app.get("/", (req, res) => {
-    res.send("look for /metrics");
-  });
+app.get("/", (req, res) => {
+  res.status(404).send("<p>look for /metrics</p>");
+});
 
-  app.get("/metrics", async (req, res) => {
-    const url = `${BASE_URL}/api/miner_info/${ADDRESS}`;
-    try {
-      const response = await fetch(url);
-      const json = await response.json();
-      const lastShareHeight = json.last_share_height;
-      const lastShareTime = json.last_share_timestamp;
-      const blocks = json.shares.blocks;
-      const uncles = json.shares.uncles;
-      lastShareHeightGauge.set(lastShareHeight);
-      lastShareTimeGauge.set(lastShareTime);
-      blocksGauge.set(blocks);
-      unclesGauge.set(uncles);
-      res.set("Content-Type", promClient.register.contentType);
-      res.end(await promClient.register.metrics());
-    } catch (error) {
-      res.status(500).send(json.error);
-    }
-  });
-};
-
-main();
+app.get("/metrics", async (req, res) => {
+  const url = `${BASE_URL}/api/miner_info/${ADDRESS}`;
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    const lastShareHeight = json.last_share_height;
+    const lastShareTime = json.last_share_timestamp;
+    const blocks = json.shares.blocks;
+    const uncles = json.shares.uncles;
+    lastShareHeightGauge.set(lastShareHeight);
+    lastShareTimeGauge.set(lastShareTime);
+    blocksGauge.set(blocks);
+    unclesGauge.set(uncles);
+    res.set("Content-Type", promClient.register.contentType);
+    res.end(await promClient.register.metrics());
+  } catch (error) {
+    res.status(500).send(json.error);
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`hundehausen/p2pool-exporter serving on :${PORT}`)
